8929521c53c7ded4d68977f03d3740892584d538
ZweiAlex <103439520+ZweiAlex@users.noreply.github.com>
2022-04-11 11:23:49 -0400

    Update even-fibonacci-numbers-sum.js
    Removed redundant holder code and declared it in loop instead

--- even-fibonacci-numbers-sum.js
+++ even-fibonacci-numbers-sum.js
@@ -1,14 +1,13 @@
 function fiboEvenSum(n) {
   let sum = 0;
   let previous = 1;
-  let holder = 1;
 
   let i = 1;
   while(i <= n){
     if(i % 2 == 0){
       sum += i
     }
-    holder = i;
+    let holder = i;
     i += previous
     previous = holder;
   }
